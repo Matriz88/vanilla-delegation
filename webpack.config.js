cc945d5476d48ff20848988c87dd3209fd31e803
Matriz <matriz@hotmail.it>
2020-02-17 21:44:04 +0100

    update dependencies

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,6 @@
 const path = require('path');
 const TerserPlugin = require('terser-webpack-plugin');
-const CleanWebpackPlugin = require('clean-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const webpackMerge = require('webpack-merge');
 
 const config = {
@@ -29,72 +29,72 @@ const config = {
 
 const delegationConfig = webpackMerge(
   config, {
-    name: 'vanilla-delegation',
-    entry: {
-      'vanilla-delegation': './vanilla-delegation.js',
-      'vanilla-delegation.min': './vanilla-delegation.js',
-    },
-    output: {
-      path: path.resolve(__dirname, 'dist'),
-      filename: '[name].js'
-    },
-    module: {
-      rules: [
-        {
-          test: /\.js$/,
-          exclude: /node_modules|dist/,
-          loader: 'eslint-loader',
+  name: 'vanilla-delegation',
+  entry: {
+    'vanilla-delegation': './vanilla-delegation.js',
+    'vanilla-delegation.min': './vanilla-delegation.js',
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules|dist/,
+        loader: 'eslint-loader',
+      }
+    ]
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+  ],
+  optimization: {
+    minimizer: [
+      new TerserPlugin({
+        include: /\.min\.js$/,
+        terserOptions: {
+          keep_fargs: true
         }
-      ]
-    },
-    plugins: [
-      new CleanWebpackPlugin(),
-    ],
-    optimization: {
-      minimizer: [
-        new TerserPlugin({
-          include: /\.min\.js$/,
-          terserOptions: {
-            keep_fargs: true
-          }
-        })
-      ]
-    }
+      })
+    ]
   }
+}
 );
 const exampleConfig = webpackMerge(
   config, {
-    name: 'example',
-    entry: {
-      index: './extras/example/src/index.js'
-    },
-    plugins: [
-      new CleanWebpackPlugin(),
-    ],
-    output: {
-      path: path.resolve('./extras/example/dist')
-    },
-    optimization: {
-      minimize: false
-    }
+  name: 'example',
+  entry: {
+    index: './extras/example/src/index.js'
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+  ],
+  output: {
+    path: path.resolve('./extras/example/dist')
+  },
+  optimization: {
+    minimize: false
   }
+}
 );
 const perfConfig = webpackMerge(
   config, {
-    name: 'perf-test',
-    entry: {
-      index: './extras/perf-test/src/index.js'
-    },
-    plugins: [
-      new CleanWebpackPlugin(),
-    ],
-    output: {
-      path: path.resolve('./extras/perf-test/dist')
-    },
-    optimization: {
-      minimize: false
-    }
+  name: 'perf-test',
+  entry: {
+    index: './extras/perf-test/src/index.js'
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+  ],
+  output: {
+    path: path.resolve('./extras/perf-test/dist')
+  },
+  optimization: {
+    minimize: false
   }
+}
 );
 
 // Return Array of Configurations
