777f9636194a20d70589223cde28cdc208e1473b
Matriz <matriz@hotmail.it>
2020-07-07 23:29:19 +0200

    tests

--- __tests__/getMatchedElement.test.js
+++ __tests__/getMatchedElement.test.js
@@ -12,17 +12,6 @@ describe('_getMatchedElement test', () => {
     `;
   });
 
-  test('matching not found, return false', () => {
-    const attachedElement = document.querySelector('div');
-    const element = document.querySelector('a');
-    const selector = 'section';
-
-    let result = _sut(attachedElement, element, selector);
-
-    // asserts
-    expect(result).toBeFalsy();
-  });
-
   test('matching found, element found returned', () => {
     const attachedElement = document.querySelector('div');
     const element = document.querySelector('span');
@@ -34,12 +23,54 @@ describe('_getMatchedElement test', () => {
     expect(result).toEqual(document.querySelector('a'));
   });
 
+  test('matching found, document is attachedElement, element found returned', () => {
+    const attachedElement = document;
+    const element = document.querySelector('span');
+    const selector = 'span';
+
+    let result = _sut(attachedElement, element, selector);
+
+    // asserts
+    expect(result).toEqual(document.querySelector('span'));
+  });
+
+  test('matching found, attachedElement is element, element found returned', () => {
+    const element = document.querySelector('span');
+    const selector = 'span';
+
+    let result = _sut(element, element, selector);
+
+    // asserts
+    expect(result).toEqual(document.querySelector('span'));
+  });
+
+  test('matching not found, return false', () => {
+    const attachedElement = document.querySelector('div');
+    const element = document.querySelector('a');
+    const selector = 'section';
+
+    let result = _sut(attachedElement, element, selector);
+
+    // asserts
+    expect(result).toBeFalsy();
+  });
+
   test('matching not found, document is attachedElement, return false', () => {
     const attachedElement = document;
     const element = document.querySelector('span');
     const selector = 'section';
 
     let result = _sut(attachedElement, element, selector);
+
+    // asserts
+    expect(result).toBeFalsy();
+  });
+
+  test('matching not found, attachedElement is element, return false', () => {
+    const element = document.querySelector('span');
+    const selector = 'section';
+
+    let result = _sut(element, element, selector);
 
     // asserts
     expect(result).toBeFalsy();
